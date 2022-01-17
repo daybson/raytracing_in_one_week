9e6f8671cfdef90193283e92249e4e1264bed895
Daybson Paisante <daybson.paisante@outlook.com>
2022-01-17 20:22:48 -0300

    + cap 1-3

---
+++ cap1-3/RayTracer/RayTracer.cpp
@@ -0,0 +1,44 @@
+// RT1W.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "vec3.h"
+#include "color.h"
+
+int main()
+{
+	// Image 
+	const int image_width = 256;
+	const int image_height = 256;
+
+	//Render
+	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; j--)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+		for (int i = 0; i < image_width; i++)
+		{
+			color pixel_color(
+				double(i) / (image_width - 1),
+				double(j) / (image_height - 1),
+				0.25);
+
+			write_color(std::cout, pixel_color);
+		}
+	}
+
+	std::cerr << "\nDone.\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
