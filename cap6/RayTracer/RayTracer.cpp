1af74f489bb28f0ed87a5ca0017c2c18c4d08b4b
Daybson Paisante <daybson.paisante@outlook.com>
2022-01-18 20:05:00 -0300

    + cap 6

---
+++ cap6/RayTracer/RayTracer.cpp
@@ -0,0 +1,80 @@
+// RT1W.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "rtweekend.h"
+#include "color.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+#include <iostream>
+
+color ray_color(const ray& r, const hittable& world)
+{
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec))
+		return 0.5 * (rec.normal + color(1, 1, 1));
+
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+	// Image 
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+	// Camera
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner =
+		origin -
+		horizontal / 2 -
+		vertical / 2 -
+		vec3(0, 0, focal_length);
+
+	//Render
+	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; j--)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+		for (int i = 0; i < image_width; i++)
+		{
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+
+			color pixel_color = ray_color(r, world);
+
+			write_color(std::cout, pixel_color);
+		}
+	}
+
+	std::cerr << "\nDone.\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
